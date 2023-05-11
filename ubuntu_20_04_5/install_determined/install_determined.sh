dc3a2a24a49edcbc98aa6335348f8dcafb7d5da7
Mahmoud Sehsah <mahmoud.rasmy.fathy1@gmail.com>
2023-05-11 01:33:35 +0000

    update script

--- ubuntu_20_04_5/install_determined/install_determined.sh
+++ ubuntu_20_04_5/install_determined/install_determined.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 sudo apt-get update && sudo apt-get install -y software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
